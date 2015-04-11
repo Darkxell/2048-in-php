6f53ce7b5c42ae72578f32d66599350b6889d310
Darkxell <darkxell.mc@gmail.com>
2015-04-11 22:46:52 +0200

    Initial submit

---
+++ 2048.php
@@ -0,0 +1,121 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>
+		2048 in php
+	</title>
+	<style type="text/css">
+		body{
+			background-color:#dedede;
+		}
+		footer.main{
+			margin-top:65px;
+			background-color:#f9f9f9;
+			height:25px;
+			width:100%;
+			text-align:right;
+			box-shadow: -1px 2px 5px 1px rgba(0, 0, 0, 0.7);
+		}
+		header.main{
+			height:100px;
+			background-color:#f9f9f9;
+			margin-bottom:65px;
+			box-shadow: -1px 2px 5px 1px rgba(0, 0, 0, 0.7);
+			text-align:center;
+		}
+		header.main p{
+			font-size:16px;
+		}
+		header.sub{
+			margin-left:10px;
+			margin-right:10px;
+			height:85px;
+			background-color:Black;
+		}
+		header.sub span{
+			
+		}
+		th.g{
+			height:80px;
+			width:80px;
+			background-color:#efefef;
+			border-radius: 15px;
+			font-size:25px;
+		}
+		div.page{
+			width:700px;
+			margin-right:auto;
+			margin-left:auto;
+			background-color:#fcfcfc;
+			box-shadow: -1px 2px 5px 1px rgba(0, 0, 0, 0.7);
+		}
+		p.easteregg{
+			color:#fcfcfc;
+			font-size:9px;
+		}
+		
+		
+		
+	</style>
+</head>
+<body>
+	<header class="main">
+		<h1>
+			2048 in php
+		</h1>
+		<p>
+			No javascript, no Flash
+		</p>
+	</header>
+	<div class="page">
+		<p class="easteregg">
+			You found an easter egg. How lucky!
+		</p>
+		<header class="sub">
+			<span>
+				2048
+			</span>
+			
+			
+		</header>
+		<br/>
+		<div class="grid">
+		<table>
+			<tr>
+				<th class="g">2</th>
+				<th class="g"></th>
+				<th class="g">2</th>
+				<th class="g"></th>
+			</tr>
+			<tr>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+			</tr>
+			<tr>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+			</tr>
+			<tr>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+				<th class="g"></th>
+			</tr>
+		</table>
+		</div>
+	</div>
+	
+	
+	
+	<footer class="main">
+		<p>
+			Code by Darkxell, open source <a href="">here</a>.
+		</p>
+	</footer>
+</body>
+</html>
